95a9ad2c50e103eefc943f448b02d484da29f692
Cristiano Alves da Silva Junior <cristiano.junior@mbra.com.br>
2018-10-22 16:25:45 -0300

    Looping through both files correctly

--- p506/main.cbl
+++ p506/main.cbl
@@ -19,6 +19,9 @@
        SELECT LISTAGEM-CONTROLE
            ASSIGN TO '.\LISTAGEM-CONTROLE.TXT'
            ORGANIZATION IS LINE SEQUENTIAL.
+       SELECT MESTRE-VENDAS-ATUAL
+           ASSIGN TO '.\MESTRE-VENDAS-ATUAL.CBDB'
+           ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA DIVISION.
        FILE SECTION.
@@ -32,7 +35,7 @@
            05 PIC X(6).
            05 VALORES-PER-ATUAL.
               10 VENDAS PIC 9999V99.
-              10 VENDAS PIC 9999V99.
+              10 COMISSAO PIC 9999V99.
            05 PIC X(3).
 
        FD TRANS-VENDAS LABEL RECORDS ARE STANDARD.
@@ -43,6 +46,11 @@
 
        FD LISTAGEM-CONTROLE LABEL RECORDS ARE OMITTED.
        01 REG-LISTAGEM-CONTROLE PIC X(90).
+
+       FD MESTRE-VENDAS-ATUAL LABEL RECORDS ARE OMITTED.
+       01 REG-MESTRE-VENDAS-ATUAL PIC X(70).
+
+
 
        WORKING-STORAGE SECTION.
       * tela
@@ -69,7 +77,7 @@
        01 REG-MESTRE-OUT.
            05 PIC XX VALUE '| '.
            05 NR-VENDEDOR-OUT PIC X(5).
-           05 PIC X(3) VALUE '      | '.
+           05 PIC X(8) VALUE '      | '.
            05 VALORES-ANO-ANT-OUT.
                10 VENDAS-OUT PIC $Z,ZZZ.99.
                10 PIC XXX VALUE ' | '.
@@ -91,6 +99,7 @@
        01 WS-ULTIMO-REGISTRO PIC X VALUE 'N'.
        01 WS-REGISTROS-POR-PAGINA PIC 99.
        01 WS-NR-PAG PIC 999 VALUE 1.
+       01 WS-MUDOU-REGISTRO-ATUAL PIC X VALUE 'N'.
 
        PROCEDURE DIVISION.
       ******************************************************************
@@ -101,8 +110,20 @@
            OPEN INPUT MESTRE-VENDAS
            OPEN INPUT TRANS-VENDAS
            OPEN OUTPUT LISTAGEM-CONTROLE
+           OPEN OUTPUT MESTRE-VENDAS-ATUAL
            PERFORM ESCREVER-CABECALHO
-           CLOSE MESTRE-VENDAS TRANS-VENDAS LISTAGEM-CONTROLE
+           PERFORM SETUP-ATUALIZAR-REGISTROS
+           PERFORM UNTIL WS-ULTIMO-REGISTRO = 'S'
+               READ TRANS-VENDAS
+                   AT END
+                       MOVE 'S' TO WS-ULTIMO-REGISTRO
+                   NOT AT END
+                       PERFORM ATUALIZAR-REGISTROS
+           END-PERFORM
+           CLOSE MESTRE-VENDAS
+                 TRANS-VENDAS
+                 LISTAGEM-CONTROLE
+                 MESTRE-VENDAS-ATUAL
            STOP RUN.
 
       ******************************************************************
@@ -126,5 +147,27 @@
            WRITE REG-LISTAGEM-CONTROLE
                FROM CABECALHO-RELATORIO-4
                AFTER ADVANCING 1 LINE.
+
+
+      ******************************************************************
+      * prepara o ambiente para a leitura dos registros de transacoes
+      ******************************************************************
+       SETUP-ATUALIZAR-REGISTROS.
+           READ MESTRE-VENDAS.
+
+
+      ******************************************************************
+      * atualiza o registro mestre dependendo da transacao atual
+      ******************************************************************
+       ATUALIZAR-REGISTROS.
+           IF NR-VENDEDOR-IN IS NOT EQUAL TO NR-VENDEDOR THEN
+               WRITE REG-MESTRE-VENDAS-ATUAL
+                   FROM REG-MESTRE-VENDAS
+                   AFTER ADVANCING 1 LINE
+              PERFORM SETUP-ATUALIZAR-REGISTROS
+           END-IF
+           ADD VENDAS-IN TO VENDAS OF VALORES-PER-ATUAL
+           ADD COMISSAO-IN TO COMISSAO OF VALORES-PER-ATUAL.
+
 
        END PROGRAM PAGE-506.
